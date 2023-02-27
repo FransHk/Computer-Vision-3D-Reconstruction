a205439f3402843a1ba5553e7fdddaaaa1cd37ad
Frans <frans-huntink@live.nl>
2023-02-27 19:13:13 +0100

    more work

--- assignment.py
+++ assignment.py
@@ -3,6 +3,8 @@ import numpy as np
 import cv2 as cv2
 block_size = 1.0
 active_voxels = []
+rotMatList = []
+tvecList = []
 
 class CamData:
     mtx = None
@@ -27,8 +29,17 @@ def LoadData():
         cam_2.mtx, cam_2.dist = [X[i] for i in ('mtx','dist')]
     with np.load('calibrations/extrinsic_cam2.npz') as X:
         cam_2.pos, cam_2.rvec, cam_2.tvec, cam_2.rotMat = [X[i] for i in ('pos', 'rvec', 'tvec', 'rotMat')]
+
     print(cam_1.pos)
     print(cam_2.pos)
+    rotMatList.append(cam_1.rotMat)
+    rotMatList.append(cam_2.rotMat)
+    rotMatList.append(cam_2.rotMat)
+    rotMatList.append(cam_2.rotMat)
+    tvecList.append(cam_1.tvec)
+    tvecList.append(cam_2.tvec)
+    tvecList.append(cam_2.tvec)
+    tvecList.append(cam_2.tvec)
     return cam_1, cam_2
 
 
@@ -65,17 +76,27 @@ def get_cam_positions():
     # Generates dummy camera locations at the 4 corners of the room
     # TODO: You need to input the estimated locations of the 4 cameras in the world coordinates.
     return np.array([[1479.83871125, 3780.60917984, -829.51554058],
-                    [302.73434419, 3159.49229733,-590.67064462]])
+                    [302.73434419, 3159.49229733,-590.67064462]])/50
 
 
 def get_cam_rotation_matrices():
+    global a, b
     # Generates dummy camera rotation matrices, looking down 45 degrees towards the center of the room
     # TODO: You need to input the estimated camera rotation matrices (4x4) of the 4 cameras in the world coordinates.
     cam_angles = [[0, 45, -45], [0, 135, -45], [0, 225, -45], [0, 315, -45]]
-    rot_1 = glm.mat4(1) # TODO ADD CAM ROTATIONS HERE
-    cam_rotations = [glm.mat4(1), glm.mat4(1), glm.mat4(1), glm.mat4(1)]
+    cam_rotations = []
 
+    # cam_rotations = [glm.mat4(1), glm.mat4(1), glm.mat4(1), glm.mat4(1)]
+    for rotmtx in rotMatList:
+        mat = glm.mat4(
+            [[rotmtx[0][0], rotmtx[0][1], rotmtx[0][2], 0],
+            [rotmtx[1][0], rotmtx[1][1], rotmtx[1][2], 0],
+            [rotmtx[2][0], rotmtx[2][1], rotmtx[2][2], 0],
+            [0, 0, 0, 1]])
+        rot_mat = glm.rotate(mat, glm.radians(90), (0, 1, 1))
+        cam_rotations.append(mat)
 
+    cam_angles = tvecList
 
     for c in range(len(cam_rotations)):
         cam_rotations[c] = glm.rotate(cam_rotations[c], cam_angles[c][0] * np.pi / 180, [1, 0, 0])
@@ -96,6 +117,7 @@ def is_in_foreground(table_element, frame_num):
     c_1 = table_element[1].astype('int')
     c_2 = table_element[2].astype('int')
 
+    # Check pixel value of each camera coord
     val_c1 = cframe_c1[c_1[0], c_1[1]]
     val_c2 = cframe_c1[c_2[0], c_2[1]]
     if val_c2 != 0 and val_c1 != 0:
@@ -106,14 +128,11 @@ def is_in_foreground(table_element, frame_num):
 def construct_table():
     global active_voxels
     global a, b
-    w = 128
-    h = 64
-    d = 128
+
     table = []
     for x in range(-10, 200, 5):
         for y in range(0, 100, 5):
             for z in range(-10, 200, 5):
-                #active_voxels.append(np.float32([x,y,z]))
                 voxel_coords = np.float32([x,y,z])
                 imgpts_a, jac = cv2.projectPoints(voxel_coords, a.rvec, a.tvec, a.mtx, a.dist)
                 imgpts_b, jac = cv2.projectPoints(voxel_coords, b.rvec, b.tvec, b.mtx, b.dist)
